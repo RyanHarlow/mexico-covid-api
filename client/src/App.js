a05f46eb299f6eb7b9ba13a42ec868ca9f3e193e
ryanharlow <ryanharlow95@gmail.com>
2020-03-27 13:55:47 -0600

    add header

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 import Map from './components/Map';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import 'bulma/css/bulma.css'
+import Header from './components/Header'
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Map />
     </div>
   );
---
+++ client/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header(props){
+    return(
+<nav className="navbar is-black" role="navigation" aria-label="main navigation">
+    <div className='container'>
+  <div className="navbar-brand">
+  <a className="navbar-item" href="https://bulma.io">
+  <h3 style={{color: 'white'}} class="title is-3">Coronavirus Tracker Mexico</h3>
+    </a>
+  </div>
+  </div>
+</nav>
+    );
+}
+
+export default Header;

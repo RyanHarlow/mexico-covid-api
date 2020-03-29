09a10598861e007769b306d3af0df9a273c5c169
ryanharlow <ryanharlow95@gmail.com>
2020-03-28 18:56:13 -0600

    add total cases and total deaths to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
 
 function Header(props) {
+
+  const [totalDeaths, setTotalDeaths] = useState(null);
+  const [totalCases, setTotalCases] = useState(null);
+  
+  useEffect(() => {
+    axios.get('/api/total')
+    .then(res => {
+      setTotalDeaths(res.data.totalDeaths);
+      setTotalCases(res.data.totalCases);
+    })
+  }, [])
+
+
+
   return (
     <nav className="navbar is-black" role="navigation" aria-label="main navigation">
       <div className='container' style={{ display: 'flex', flexDirection: 'column' }}>
@@ -10,9 +26,8 @@ function Header(props) {
           </a>
         </div>
         <div style={{margin:0,padding:0}}>
-          <p style={{margin:0,padding:0}}>total deaths;lkjfal;fjakl;</p>
-          <p>total caseslkjad;kf</p>
-          <p>asdfdsf</p>
+  <p>Total Deaths: {totalDeaths}</p>
+  <p>Total Cases: {totalCases}</p>
         </div>
       </div>
     </nav>

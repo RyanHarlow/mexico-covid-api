6528d4085ddc4123efaf3cc0b9a415b93995a7c4
ryanharlow <ryanharlow95@gmail.com>
2020-03-28 18:49:28 -0600

    fix tooltip

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
-function Header(props){
-    return(
-<nav className="navbar is-black" role="navigation" aria-label="main navigation">
-    <div className='container'>
-  <div className="navbar-brand">
-  <a className="navbar-item" href="https://bulma.io">
-  <h3 style={{color: 'white', fontSize:'15px'}} class="title is-3">Coronavirus Tracker Mexico</h3>
-    </a>
-  </div>
-  </div>
-</nav>
-    );
+function Header(props) {
+  return (
+    <nav className="navbar is-black" role="navigation" aria-label="main navigation">
+      <div className='container' style={{ display: 'flex', flexDirection: 'column' }}>
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io" style={{ margin: 0, padding: 0 }}>
+            <h3 style={{ color: 'white', fontSize: '20px'}} class="title is-3"><strong>Coronavirus Tracker Mexico</strong></h3>
+          </a>
+        </div>
+        <div style={{margin:0,padding:0}}>
+          <p style={{margin:0,padding:0}}>total deaths;lkjfal;fjakl;</p>
+          <p>total caseslkjad;kf</p>
+          <p>asdfdsf</p>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
-import MapGL, {Source, Layer} from 'react-map-gl';
+import React, { Component } from 'react';
+import MapGL, { Source, Layer } from 'react-map-gl';
 import axios from 'axios';
-import {dataLayer} from './map-style.js';
-import {updatePercentiles} from './utils'
+import { dataLayer } from './map-style.js';
+import { updatePercentiles } from './utils'
 
 const MAPBOX_TOKEN = 'pk.eyJ1Ijoicnlhbmhhcmxvdzk1IiwiYSI6ImNrNTJ1c2NtbDAxdzczZ250eGZ5ZXYyY2gifQ.2EN37mO-Fn8QNmu-FoOFDA'; // Set your mapbox token here
 
 
-class Map extends Component{
+class Map extends Component {
 
   state = {
     data: null,
@@ -22,11 +22,11 @@ class Map extends Component{
 
   componentDidMount() {
     axios.get('/api/geojson')
-    .then(res => {
-       let data = res.data;
-       console.log(data)
-      this._loadData(data);
-    })
+      .then(res => {
+        let data = res.data;
+        console.log(data)
+        this._loadData(data);
+      })
   }
 
   _loadData = data => {
@@ -35,25 +35,25 @@ class Map extends Component{
     });
   };
 
-  _onViewportChange = viewport => this.setState({viewport});
+  _onViewportChange = viewport => this.setState({ viewport });
 
 
   _onClick = event => {
     const {
       features,
-      srcEvent: {offsetX, offsetY}
+      srcEvent: { offsetX, offsetY }
     } = event;
     const clickedFeature = features && features.find(f => f.layer.id === 'data');
 
-    this.setState({clickedFeature, x: offsetX, y: offsetY});
+    this.setState({ clickedFeature, x: offsetX, y: offsetY });
   };
 
   _renderTooltip() {
-    const {clickedFeature, x, y} = this.state;
+    const { clickedFeature, x, y } = this.state;
 
     return (
       clickedFeature && (
-        <div className="tooltip" style={{left: x, top: y, margin:'1em', fontSize:'20px', backgroundColor: 'white', display:'inline-block', padding: '1rem', border:'1px solid black', borderRadius:'1em'}}>
+        <div  className="tooltip" style={{position:'absolute', top:'10px', left:'10px', margin: '.5em', fontSize: '20px', backgroundColor: 'white', display: 'inline-block', padding: '1rem', border: '1px solid black', borderRadius: '1em', zIndex: 1000 }}>
           <div>State: {clickedFeature.properties.name}</div>
           <div>Number Of Cases: {clickedFeature.properties.totalCases}</div>
           <div>Number Of Deaths: {clickedFeature.properties.totalDeaths}</div>
@@ -62,11 +62,11 @@ class Map extends Component{
     );
   }
 
-  render(){
-    const {viewport, data} = this.state;
-
-    return(
-      <div style={{height: '100%', width: '100%', position: 'relative'}}>
+  render() {
+    const { viewport, data } = this.state;
+    console.log(JSON.stringify(this.state.clickedFeature))
+    return (
+      <div style={{ height: '100%', width: '100%', position: 'relative' }}>
         <MapGL
           {...viewport}
           width="100vw"
@@ -79,8 +79,9 @@ class Map extends Component{
           <Source type="geojson" data={data}>
             <Layer {...dataLayer} />
           </Source>
-          {this._renderTooltip()}
+          
         </MapGL>
+        {this._renderTooltip()}
       </div>
     )
   }

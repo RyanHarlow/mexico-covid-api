f8cce3408379b014bf80312fcb38ccc27e651d92
ryanharlow <ryanharlow95@gmail.com>
2020-04-08 19:58:27 -0500

    update data and gender from abbreviation

--- app.js
+++ app.js
@@ -79,9 +79,9 @@ app.get('/api/total', (req, res) => {
     let totalM = 0;
     let ageCount = 0;
     for(let i = 0; i < data.length; i++){
-        if(data[i].sex == 'F'){
+        if(data[i].sex == 'FEMENINO'){
             totalF++;
-        }if(data[i].sex == 'M'){
+        }if(data[i].sex == 'MASCULINO'){
             totalM++;
         }
         ageCount += Number(data[i].age);
@@ -107,9 +107,9 @@ app.get('/api/state/:state', (req, res) => {
             ageCount += Number(data[i].age);
             cases.push(data[i]);
             total.totalCases++;
-            if(data[i].sex == 'M'){
+            if(data[i].sex == 'MASCULINO'){
                 total.totalM++;
-            }else if(data[i].sex == 'F'){
+            }else if(data[i].sex == 'FEMENINO'){
                 total.totalF++;
             }
         }
